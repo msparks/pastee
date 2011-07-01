7570e41b877101ce5c021a1795c1e9ed0e45f929
Matt Sparks <ms@quadpoint.org>
2011-07-01 04:36:06 -0700

    Adding Datastore class and tests

--- backend/pastee/datastore.py
+++ backend/pastee/datastore.py
@@ -1,6 +1,112 @@
 import redis
 
 
+class Datastore(object):
+  '''Abstract wrapper for a key-value store.'''
+  def __init__(self):
+    '''Constructor.'''
+    self._conn = redis.Redis()
+    self._prefix = u''
+
+  def _key(self, key):
+    '''Returns 'key' with the prefix prepended.'''
+    return u'%s:%s' % (self._prefix, key)
+
+  def prefix(self):
+    '''Returns the key prefix.'''
+    return self._prefix
+
+  def prefix_is(self, prefix):
+    '''Sets the key prefix.
+
+    All keys will be prefixed with this value and a colon before being inserted
+    into the underlying datastore. Other methods use the prefix to set and get
+    keys from the datastore.
+
+    Args:
+      prefix: new prefix to use
+    '''
+    self._prefix = prefix
+
+  def value(self, key):
+    '''Returns the value with given key, or None if the key does not exist.
+
+    Args:
+      key: key of value to retrieve
+
+    Returns:
+      string value or None
+    '''
+    value = self._conn.get(self._key(key))
+    if value:
+      value = value.decode('utf-8')
+    return value
+
+  def value_is(self, key, value):
+    '''Sets the value for the key in the datastore.
+
+    Args:
+      key: string key
+      value: stringifiable value
+    '''
+    self._conn.set(self._key(key), value)
+
+  def nxvalue_is(self, key, value):
+    '''Sets the value for 'key' if 'key' does not exist.
+
+    Args:
+      key: string key
+      value: stringifiable value
+
+    Raises:
+      KeyError if the given key already exists. In this case, the existing
+      value is not modified.
+    '''
+    success = self._conn.setnx(self._key(key), value)
+    if not success:
+      raise KeyError, 'key already exists'
+
+  def delete(self, key):
+    '''Deletes a key.
+
+    Args:
+      key: key to delete
+
+    Returns:
+      True on success (key existed)
+    '''
+    return self._conn.delete(self._key(key))
+
+  def keys(self, pattern=None):
+    '''Returns a list of keys matching pattern.
+
+    Args:
+      pattern: string pattern; can have '*' and '?'
+
+    Returns:
+      list of matching keys
+    '''
+    if pattern is None:
+      pattern = '*'
+    keys = self._conn.keys(self._key(pattern))
+
+    # Remove prefix from all keys.
+    prefix = u'%s:' % self._prefix
+    keys = [x.decode('utf-8')[len(prefix):] for x in keys]
+    return keys
+
+  def exists(self, key):
+    '''Check if a key exists.
+
+    Args:
+      key: key to check for existence
+
+    Returns:
+      True if key exists
+    '''
+    return self._conn.exists(self._key(key))
+
+
 _ds_conn = None
 
 
---
+++ backend/tests/datastore_test.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from nose.tools import *
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from pastee import datastore
+
+
+class Test_Datastore:
+  '''Test for pastee.datastore.
+
+  This does both unit tests of code as well as live tests on a real
+  datastore. A running instance of Redis is needed to pass the live tests in
+  this suite.
+  '''
+  def setup(self):
+    self._ds = datastore.Datastore()
+    self._testing_prefix = 'pastee:test'
+
+  def test_construct(self):
+    '''Simple construction test'''
+    assert_equal(type(self._ds), datastore.Datastore)
+
+  def test_prefix(self):
+    '''Set and get prefix'''
+    # Empty prefix by default.
+    assert_equal(self._ds.prefix(), '')
+
+    # Check ascii and unicode strings.
+    prefixes = ('foo', '1234', 'baz1234', u'føøbár')
+    for prefix in prefixes:
+      self._ds.prefix_is(prefix)
+      assert_equal(self._ds.prefix(), prefix)
+      assert_equal(self._ds._key('bar'), u'%s:bar' % prefix)
+
+  def test_value(self):
+    '''Live test: value() and value_is()'''
+    # Use a specific testing prefix so we can clean up.
+    self._ds.prefix_is(self._testing_prefix)
+
+    # Ensure we can set and retrieve values, including unicode.
+    kv = {'foo': 'bar',
+          'baz1': 'quux1',
+          'unicode!': u'føø/bár',
+          u'føø/bárkey': 'string value'}
+    for (key, value) in kv.items():
+      self._ds.value_is(key, value)
+      assert_equal(self._ds.value(key), value)
+
+  def test_nxvalue(self):
+    '''Live test: nxvalue_is()'''
+    # Create a key.
+    key = 'my_foo_key'
+    self._ds.value_is(key, 'value')
+
+    # nxvalue_is should fail because key exists.
+    assert_raises(KeyError, self._ds.nxvalue_is, key, 'new value')
+
+    # It should succeed on a non-existent key.
+    self._ds.nxvalue_is('another key', 'another value')
+
+  def test_exists(self):
+    '''Live test: exists()'''
+    keys = ('foo', 'bar', 'baz', 'quux', u'føø/bárkey')
+    for key in keys:
+      self._ds.value_is(key, 'value')
+      assert_true(self._ds.exists(key))
+      assert_true(key in self._ds.keys())
+
+  def test_delete(self):
+    '''Live test: delete()'''
+    keys = ('foo', 'bar', 'baz', 'quux', u'føø/bárkey')
+    for key in keys:
+      self._ds.value_is(key, 'value')
+      assert_true(self._ds.exists(key))
+      assert_true(self._ds.delete(key))
+      assert_false(self._ds.exists(key))
+
+  def teardown(self):
+    '''Clean up.'''
+    keys = self._ds.keys()  # only keys starting with the testing prefix
+    for key in keys:
+      self._ds.delete(key)
