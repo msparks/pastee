a9df22e08458a4564f808e2afae1768add491254
Matt Sparks <ms@quadpoint.org>
2011-07-04 00:01:58 -0700

    Use paste.Paste interface in views

--- backend/pastee/views.py
+++ backend/pastee/views.py
@@ -7,6 +7,7 @@ from django import http
 import datastore
 import formatting
 import idmanager
+import paste
 
 JSON_CONTENT_TYPE = 'application/json'
 MAX_LENGTH = 32 * 1024    # 32 KiB
@@ -35,17 +36,15 @@ def get(request, id, mode=None):
   if len(id) > 100:
     return error_response('Invalid ID', status=404)
 
-  # Look up metadata.
+  # Lookup saved paste.
   # TODO(ms): Handle expired posts.
-  md_key = 'metadata:%s' % id
-  md_json = DS.value(md_key)
-  if md_json is None:
+  try:
+    saved_paste = paste.Paste(DS, id=id)
+  except KeyError:
     return error_response('Invalid ID', status=404)
-  md = json.loads(md_json)
 
   # Decode content.
-  content_key = 'content:%s' % id
-  content = DS.value(content_key)
+  content = saved_paste.content()
   if content is None:
     return error_response('Expired', status=403)
 
@@ -56,7 +55,7 @@ def get(request, id, mode=None):
     # Download.
 
     # Lookup file extension for lexer.
-    lexer_alias = md.get('lexer', 'text')
+    lexer_alias = saved_paste.lexer_alias()
     filename = 'pastee-%s.%s' % (id, formatting.lexer_ext(lexer_alias))
 
     response = http.HttpResponse(content,
@@ -65,13 +64,13 @@ def get(request, id, mode=None):
     return response
   else:
     # HTML-ized output.
-    lexer_alias = formatting.validate_lexer_name(md.get('lexer', 'text'))
+    lexer_alias = formatting.validate_lexer_name(saved_paste.lexer_alias())
     html = formatting.htmlize(content, lexer_alias)
     data = {'id': id,
-            'created': md.get('created', 0),
+            'created': saved_paste.created(),
             'lexer': formatting.lexer_longname(lexer_alias),
             'lexer_alias': lexer_alias,
-            'ttl': md.get('ttl', 0),
+            'ttl': saved_paste.ttl(),
             'html': html,
             'raw': content}
     return http.HttpResponse(json.dumps(data), content_type=JSON_CONTENT_TYPE)
@@ -127,22 +126,16 @@ def submit(request):
                              status=403,
                              content_type=JSON_CONTENT_TYPE)
 
-  # Get an ID for the new paste.
-  mgr = idmanager.IDManager(DS)
-  id = mgr.new_id()
+  # Create paste.
+  new_paste = paste.Paste(DS)
+  new_paste.ttl_is(ttl)
+  new_paste.lexer_alias_is(lexer)
+  new_paste.ip_address_is(request.META.get('REMOTE_ADDR'))
+  new_paste.content_is(content)
 
-  # Store the metadata object.
-  md_key = 'metadata:%s' % id
-  md = {'ttl': ttl,
-        'lexer': lexer,
-        'ip_address': request.META.get('REMOTE_ADDR'),
-        'created': int(time.time())}
-  DS.value_is(md_key, json.dumps(md))
+  # Save paste.
+  new_paste.save_state_is(paste.Paste.SaveStates.CLEAN)
 
-  # Store the content object.
-  content_key = 'content:%s' % id
-  DS.value_is(content_key, content)
-
-  response = {'id': id}
+  response = {'id': new_paste.id()}
   return http.HttpResponse(json.dumps(response),
                            content_type=JSON_CONTENT_TYPE)
