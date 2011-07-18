eff5290946fcc8114c875b49f88f80d9f532c765
Matt Sparks <ms@quadpoint.org>
2011-07-17 21:26:01 -0700

    Change backend to respond to /api/ directly

--- backend/pastee/urls.py
+++ backend/pastee/urls.py
@@ -2,13 +2,7 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('backend.pastee.views',
-  (r'^$', 'index'),
-  (r'^submit$', 'submit'),
-  (r'^get/(?P<id>[a-zA-Z0-9]+)(?P<mode>/(raw|download))?$', 'get'),
-
-  (r'^api$', 'api'),
-  (r'^statistics$', 'statistics'),
-  (r'^about$', 'about'),
-
-  (r'^(?P<paste_id>[a-zA-Z0-9]+)/preview$', 'preview')
+  (r'^api/$', 'index'),
+  (r'^api/submit$', 'submit'),
+  (r'^api/get/(?P<id>[a-zA-Z0-9]+)(?P<mode>/(raw|download))?$', 'get'),
 )
