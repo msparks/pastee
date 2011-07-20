3dcccec0a83ec663f0ee01c0f06af051f5eef848
Matt Sparks <ms@quadpoint.org>
2011-07-20 03:38:42 -0700

    Removing more django-specific code

--- backend/pastee/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-
-
-urlpatterns = patterns('backend.pastee.views',
-  (r'^api/$', 'index'),
-  (r'^api/submit$', 'submit'),
-  (r'^api/get/(?P<id>[a-zA-Z0-9]+)(?P<mode>/(raw|download))?$', 'get'),
-)
