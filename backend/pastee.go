46be8a1b94bae7aedac4f1659befca0ab27720d7
Matt Sparks <ms@quadpoint.org>
2014-02-02 01:05:39 -0800

    Start of /pastes handler.

--- backend/pastee.go
+++ backend/pastee.go
@@ -7,8 +7,27 @@ import (
 
 func init() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/pastes", pastesPostHandler)
+	http.HandleFunc("/pastes/", pastesGetHandler)
 }
 
 func indexHandler(w http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(w, "Index handler")
+}
+
+// Handles GET requests to /pastes/{id}.
+func pastesGetHandler(w http.ResponseWriter, request *http.Request) {
+	if request.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintf(w, "%v", request.URL)
+}
+
+func pastesPostHandler(w http.ResponseWriter, request *http.Request) {
+	if request.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 }
