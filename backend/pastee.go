d464410c47844bc24079e667ff7fac7c3a102e94
Matt Sparks <ms@quadpoint.org>
2014-02-02 03:37:30 -0800

    Add a non-trivial POST handler for /pastes.

--- backend/pastee.go
+++ backend/pastee.go
@@ -1,9 +1,28 @@
 package pastee
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
+
+type PasteCreationReq struct {
+	Content string
+	Mac     string
+	Expiry  string
+}
+
+type PasteCreationResp struct {
+	Id string `json:"id"`
+}
+
+type Paste struct {
+	content string
+	mac     string
+	expiry  time.Time
+}
 
 func init() {
 	http.HandleFunc("/", indexHandler)
@@ -22,7 +41,10 @@ func pastesGetHandler(w http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", request.URL)
+	// Extract id from URL.
+	id := strings.Replace(request.URL.Path, "/pastes/", "", -1)
+
+	fmt.Fprintf(w, "%v", id)
 }
 
 func pastesPostHandler(w http.ResponseWriter, request *http.Request) {
@@ -30,4 +52,41 @@ func pastesPostHandler(w http.ResponseWriter, request *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+
+	const kMaxContentLength = 256 * 1024
+	if request.ContentLength > kMaxContentLength {
+		w.WriteHeader(http.StatusRequestEntityTooLarge)
+		return
+	}
+	if request.ContentLength < 0 {
+		w.WriteHeader(http.StatusLengthRequired)
+		return
+	}
+
+	postData := make([]byte, request.ContentLength)
+	_, err := request.Body.Read(postData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "paste must contain a body")
+		return
+	}
+
+	creationRequest, err := pasteCreationRequestFromPostData(postData)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "%v\n", err)
+		return
+	}
+
+	responseBytes, err := json.Marshal(PasteCreationResp{Id: "foo"})
+	fmt.Fprintf(w, "request: %+v\n", creationRequest)
+	fmt.Fprintf(w, "response: %+v\n", string(responseBytes))
+}
+
+func pasteCreationRequestFromPostData(postData []byte) (PasteCreationReq, error) {
+	var creationRequest PasteCreationReq
+	if err := json.Unmarshal(postData, &creationRequest); err != nil {
+		return creationRequest, err
+	}
+	return creationRequest, nil
 }
